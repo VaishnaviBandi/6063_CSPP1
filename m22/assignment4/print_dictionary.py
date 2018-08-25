336686471061212448e4b903f8f31d48753ccb50
VaishnaviBandi <vaishnavibandi9@msitprogram.net>
2018-08-25 12:35:31 +0530

    " -> 3 of 3 passed. pylint: 8.89/10 "

--- m22/assignment4/print_dictionary.py
+++ m22/assignment4/print_dictionary.py
@@ -5,11 +5,13 @@ the key and frequency with a SPACE - SPACE.
 '''
 
 def print_dictionary(dictionary):
+    """function to print a dictionary with the keys in sorted order"""
     dict1 = sorted(dictionary.keys())
     for word in dict1:
         print(word, '-', dictionary[word])
 
 def main():
+    """main function"""
     dictionary = eval(input())
     print_dictionary(dictionary)
 
